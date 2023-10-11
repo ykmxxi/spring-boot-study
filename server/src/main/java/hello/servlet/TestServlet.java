98178570121a13a98a64447d806615714d7f3ba8
ykmxxi <ykmxxi97@gmail.com>
2023-10-11 16:13:39 +0900

    Fix #1- remove throws servlet exception

--- server/src/main/java/hello/servlet/TestServlet.java
+++ server/src/main/java/hello/servlet/TestServlet.java
@@ -2,7 +2,6 @@ package hello.servlet;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,7 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet {
 
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		System.out.println("TestServlet.service");
 		resp.getWriter().println("test"); // HTTP 서블릿 응답에 해당 문자열을 출력
 	}
