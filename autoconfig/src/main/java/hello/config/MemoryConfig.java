d435b78158def42d95e6883d7a2ea651e0feffa7
ykmxxi <ykmxxi97@gmail.com>
2023-10-24 09:58:08 +0900

    Implement #7- `@Conditional`기능을 사용해 특정 상황에만 동작
    - `-Dmemory=on`조건에서 메모리 측정 기능 사용
    - org.springframework.context.annotation.Condition 인터페이스를 구현해야 함

--- autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,12 +1,15 @@
 package hello.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 
 @Configuration
+@Conditional(MemoryCondition.class)
 public class MemoryConfig {
 
 	@Bean
