6ef5a0fcb22eaba29c3ecf8e4a7eb4113c7634a6
ykmxxi <ykmxxi97@gmail.com>
2023-10-24 10:29:36 +0900

    Implement #7- 스프링 부트가 제공하는 `@Conditional` 관련 기능 사용
    - `@Conditional`: 스프링 프레임워크가 제공하는 기능
    - `@ConditionalOnXxx`: 스프링 부트가 제공하는 기능
    - 스프링 부트는 이미 필요한 대부분의 Condition 인터페이스 구현체를 만들어 제공

--- autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,15 +1,15 @@
 package hello.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
-import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 
 @Configuration
-@Conditional(MemoryCondition.class)
+// @Conditional(MemoryCondition.class)
+@ConditionalOnProperty(name = "memory", havingValue = "on") // 부트가 제공하는 기능
 public class MemoryConfig {
 
 	@Bean
