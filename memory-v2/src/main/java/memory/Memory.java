55c1d9599d76dc0821232a369481ed3a7f5b62ec
ykmxxi <ykmxxi97@gmail.com>
2023-10-25 10:33:53 +0900

    Implement #7- memory 기능 라이브러리 v2(자동 구성 사용)

---
+++ memory-v2/src/main/java/memory/Memory.java
@@ -0,0 +1,29 @@
+package memory;
+
+public class Memory {
+
+	private long used;
+	private long max;
+
+	public Memory(final long used, final long max) {
+		this.used = used;
+		this.max = max;
+	}
+
+	public long getUsed() {
+		return used;
+	}
+
+	public long getMax() {
+		return max;
+	}
+
+	@Override
+	public String toString() {
+		return "Memory{" +
+				"used=" + used +
+				", max=" + max +
+				'}';
+	}
+
+}
