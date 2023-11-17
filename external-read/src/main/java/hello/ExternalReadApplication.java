ea2c640d9de0cd8dd0941771d7b90202789c10e8
ykmxxi <ykmxxi97@gmail.com>
2023-11-17 16:37:07 +0900

    Implement #11- `@Value`를 통해 외부 설정 사용
    - `@Value`도 내부에서는 Environment를 사용
    - org.springframework.beans.factory.annotation.Value
    - 하나하나 외부 설정의 키 값을 입력받고, 주입 받아와야 하는 부분이 번거로움

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import hello.config.MyDataSourceEnvConfig;
+import hello.config.MyDataSourceValueConfig;
 
-@Import(MyDataSourceEnvConfig.class)
+// @Import(MyDataSourceEnvConfig.class)
+@Import(MyDataSourceValueConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
