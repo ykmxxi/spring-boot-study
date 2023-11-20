c9795fd7f957cbaf3242ad84f034e753411e5cd7
ykmxxi <ykmxxi97@gmail.com>
2023-11-20 12:18:09 +0900

    Implement #11- `@ConfigurationProperties` 검증기 사용
    - 자바 표준 검증기, 하이버네이트 검증기 사용 가능
    - 쉽고 편리하게 검증을 진행할 수 있다
    - 외부 설정을 객체로 편리하게 변환해서 사용
    - 외부 설정의 계층을 객체로 편리하게 표현 가능
    - 외부 설정을 타입 안전하게 사용

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import hello.config.MyDataSourceConfigV2;
+import hello.config.MyDataSourceConfigV3;
 
-@Import(MyDataSourceConfigV2.class)
+@Import(MyDataSourceConfigV3.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
