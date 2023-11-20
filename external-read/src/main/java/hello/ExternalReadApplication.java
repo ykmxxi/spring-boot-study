47093d471b81330a91630617142b3eac74456d32
ykmxxi <ykmxxi97@gmail.com>
2023-11-20 11:13:24 +0900

    Implement #11- `@ConfigurationProperties`를 통해 외부 설정 사용
    - 외부 설정의 묶음 정보를 객체로 변환하는 기능
    - 자바빈 프로퍼티 방식 사용
    - setter를 가지고 있기 때문에 누군가 실수로 값을 변경하는 문제가 발생할 수 있다

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import hello.config.MyDataSourceValueConfig;
+import hello.config.MyDataSourceConfigV1;
 
 // @Import(MyDataSourceEnvConfig.class)
-@Import(MyDataSourceValueConfig.class)
+// @Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV1.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
